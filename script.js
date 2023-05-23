f6bf431d33a620936bd1ef7de7ba8f1080990880
Michael Perkins <mperkins1995@gmail.com>
2023-05-22 22:50:33 -0500

    add business-plan.html and link to investor nav link

--- script.js
+++ script.js
@@ -21,3 +21,38 @@ function updateUI(id) {
     }
 
 }
+
+// wire up the form, which has an action="subscribe"
+//  <input type="submit" value="Subscribe" class="btn" />
+
+
+// Path: subscribe.js
+var subscribeForm = document.getElementById('subscribe-form');
+
+subscribeForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    // when the form is submitted, grab the email address
+    // and send an email to mperkins1995@gmail.com with the email address
+
+    var email = document.getElementById('email').value;
+
+    var data = {
+        email: email
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/subscribe', true);
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(JSON.stringify(data));
+    // make a route in app.js that will handle this request
+
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log('Email sent!');
+        }
+    }
+
+
+});
